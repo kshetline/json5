eb00019baacb832237e2d15e313ee03756b782e3
Kerry Shetline <kerry@shetline.com>
2025-06-03 19:12:10 -0400

    First pass at SUPER MEGA UPDATE!!!

--- lib/util.d.ts
+++ lib/util.d.ts
@@ -1,8 +1,9 @@
-export declare function isSpaceSeparator(c?: string): boolean
-export declare function isIdStartChar(c?: string): boolean
-export declare function isIdContinueChar(c?: string): boolean
-export declare function isDigit(c?: string): boolean
-export declare function isHexDigit(c?: string): boolean
+export declare function isSpaceSeparator(c?: number): boolean
+export declare function isIdStartChar(c?: number): boolean
+export declare function isIdContinueChar(c?: number): boolean
+export declare function isDigit(c?: number): boolean
+export declare function isHexDigit(c?: number): boolean
+export declare function setObjectProperty(obj: any, key: string | number | symbol, value: any): void
 export declare class ValueSourceWrapper {
     constructor (source: string, value: any)
 }
